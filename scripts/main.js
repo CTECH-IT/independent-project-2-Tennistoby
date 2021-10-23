67c74571d745bb560f1f5cffd21cca8fedfb8eaf
TennisToby <zoeychen2013@gmail.com>
2021-10-23 16:39:06 -0500

    created layout of page

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-// Put your JavaScript here
+const DETAIL_IMAGE_SELECTOR =  '[data-image-role="target"]';
+const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+
+function setDetails(imageUrl, titleText) {
+    'use strict';
+    let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+
+    let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent = titleText;
+}
